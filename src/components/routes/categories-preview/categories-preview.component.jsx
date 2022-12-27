28387c6b6f2ea1a24741d4785888d3113cae923a
Amir Ghanbari <81a.ghanbari@gmail.com>
2022-12-27 20:40:27 +0330

    add async redux thunk and the spinner component

--- src/components/routes/categories-preview/categories-preview.component.jsx
+++ src/components/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,30 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 
-import { selectCategoriesMap } from "../../../store/categories/category.selector";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../../store/categories/category.selector";
 
 import CategoryPreview from "../../category-preview/category-preview.component";
+import Spinner from "../../spinner/spinner.component";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <Fragment className="category-preview-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
