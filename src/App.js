a1fba2b2877d016f9cd7d47018ac585c2d7f32df
Amir Ghanbari <81a.ghanbari@gmail.com>
2022-12-29 19:43:08 +0330

    complete migration to redux saga

--- src/App.js
+++ src/App.js
@@ -7,25 +7,14 @@ import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authentication/authentication.component';
 import Shop from './components/routes/shop/shop.component'
 import Checkout from './components/routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
+import { checkUserSession } from './store/user/user.action';
+
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    dispatch(checkUserSession());
   }, [dispatch]);
 
   return (
